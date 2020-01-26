e888af588f6b4c537c37aec48eb4295e081ccf96
Vanderson Sander <vanderson_sander@hotmail.com>
2020-01-26 14:54:39 -0300

    Adicionado touchEvents

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -3,6 +3,7 @@ export default class Slide {
     this.wrapper = this.s(wrapper);
     this.slide = this.s(slide);
     this.posx = 0;
+    this.touch = { start: 0, move: 0 };
   }
 
   s(e) {
@@ -21,6 +22,7 @@ export default class Slide {
     target.removeEventListener(event, callback);
   }
 
+  // Mouse Events
   onStart(e) {
     e.preventDefault();
     this.ae(this.wrapper, 'mousemove', this.onMove);
@@ -36,18 +38,42 @@ export default class Slide {
     this.re(this.wrapper, 'mousemove', this.onMove);
   }
 
+  // Touch Events
+  touchStart(e) {
+    this.ae(this.wrapper, 'touchmove', this.touchMove);
+    this.ae(this.wrapper, 'touchend', this.touchEnd);
+    this.touch.start = e.touches[0].clientX;
+  }
+
+  touchMove(e) {
+    this.touch.move = e.touches[0].clientX - this.touch.start;
+    this.move(this.slide, this.posx + this.touch.move);
+  }
+
+  touchEnd() {
+    this.posx += this.touch.move;
+    this.re(this.wrapper, 'touchmove', this.touchMove);
+  }
+
   move(target, x) {
     target.style.transform = `translate3d(${x}px, 0, 0)`;
   }
 
   bindEvents() {
+    // Bind the Mouse Events
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
+    // Bind the Touch Events
+    this.touchStart = this.touchStart.bind(this);
+    this.touchMove = this.touchMove.bind(this);
+    this.touchEnd = this.touchEnd.bind(this);
+    // this.tMove = this.tMove.bind(this);
   }
 
   init() {
     this.bindEvents();
     this.ae(this.wrapper, 'mousedown', this.onStart);
+    this.ae(this.wrapper, 'touchstart', this.touchStart);
   }
 }
